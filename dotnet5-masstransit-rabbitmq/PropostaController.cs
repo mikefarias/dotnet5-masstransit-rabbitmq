1b4fe69cb16adb800ff701b1da3149f2cd3b743f
4042190 <mike@protechsolutions.com.br>
2021-05-28 13:04:37 -0300

    Classe controller para proposta

---
+++ dotnet5-masstransit-rabbitmq/PropostaController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet5_masstransit_rabbitmq
+{
+
+
+    [Route("api/controller")]
+    public class PropostaController : Controller
+    {
+        //private readonly DbContext _context;
+
+        public PropostaController()
+        {
+            //_context = context;
+        }
+
+        [HttpPost]
+        public ActionResult CadastrarProposta()
+        {
+
+            return Ok();
+        }
+    }
+}
